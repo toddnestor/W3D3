99f9a6409eddfbe8cdaafbb8efc8b6cbab3d5098
App Academy Student <student@appacademy.io>
2016-09-28 11:23:45 -0700

    Finished associations exercise

---
+++ associations/app/models/course.rb
@@ -0,0 +1,20 @@
+class Course < ActiveRecord::Base
+  has_many :enrollments,
+  primary_key: :id,
+  foreign_key: :course_id,
+  class_name: :Enrollment
+
+  belongs_to :prerequisite,
+  primary_key: :id,
+  foreign_key: :prereq_id,
+  class_name: :Course
+
+  belongs_to :instructor,
+  primary_key: :id,
+  foreign_key: :instructor_id,
+  class_name: :User
+
+  has_many :enrollments,
+  through: :enrollments,
+  source: :user
+end
