9d3d05fd5b81222ee18e8a6a9592192a9c23d6f7
App Academy Student <student@appacademy.io>
2016-09-28 16:02:31 -0700

    users can't submit more than 5 urls per minute

--- URLShortener/app/models/shortened_url.rb
+++ URLShortener/app/models/shortened_url.rb
@@ -1,6 +1,8 @@
 class ShortenedUrl < ActiveRecord::Base
   validates :long_url, :short_url, :user_id, presence: true
   validates :short_url, uniqueness: true
+  validates :long_url, length: {minimum: 4, maximum: 2083}
+  validate :more_than_five_urls_in_a_minute
 
   belongs_to :submitter,
     primary_key: :id,
@@ -26,6 +28,14 @@ class ShortenedUrl < ActiveRecord::Base
     through: :tags,
     source: :topic
 
+  def long_url=(url)
+    unless url.match(/^((http(s)?:)?\/\/).*/)
+      url = "http://" + url
+    end
+
+    write_attribute(:long_url, url)
+  end
+
   def self.random_code(n=8)
     code = SecureRandom::urlsafe_base64[0..n]
     return self.random_code(n) if ShortenedUrl.find_by(short_url: code)
@@ -33,7 +43,13 @@ class ShortenedUrl < ActiveRecord::Base
   end
 
   def self.create_for_user_and_long_url!(user, long_url)
-    ShortenedUrl.create!(long_url: long_url, short_url: ShortenedUrl.random_code, user_id: user.id)
+    ShortenedUrl.create(long_url: long_url, short_url: ShortenedUrl.random_code, user_id: user.id)
+  end
+
+  def more_than_five_urls_in_a_minute
+    if ShortenedUrl.where("shortened_urls.created_at > ?", 1.minute.ago).where(user_id: self.user_id).count >= 5
+      self.errors[:cannot_create] << "more than five urls in a minute"
+    end
   end
 
   def num_clicks
